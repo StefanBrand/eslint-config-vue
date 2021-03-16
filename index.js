e0b41092168b3c73a1826ca653d1c6b51bfa3e25
komninoschat <komninoschat@gmail.com>
2021-03-16 16:24:53 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true
+  },
+  extends: [
+    'plugin:vue/essential', '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'global-require': 'off',
+    'import/no-dynamic-require': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-underscore-dangle': 'off',
+    'no-prototype-builtins': 'off',
+    'no-plusplus': 'off',
+    'no-return-assign': 'off',
+    'no-mixed-operators': 'off'
+  }
+};
